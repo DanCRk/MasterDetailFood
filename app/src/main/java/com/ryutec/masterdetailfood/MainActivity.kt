9afe1a1ecd0f78b69aac26ee4ff8adaa48f6a249
DanCrk <dancrkyt@gmail.com>
2022-01-22 01:09:52 -0600

    Initial commit

---
+++ app/src/main/java/com/ryutec/masterdetailfood/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.ryutec.masterdetailfood
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.ryutec.masterdetailfood.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // settear la vista con databinding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setNavController()
+
+    }
+
+    fun setNavController (){
+        // Asignar el bottom nav
+        val btmNav = binding.bottomNav
+        // Instanciar un controllador
+        val navControler = Navigation.findNavController(this, R.id.frag_host)
+        // Settear el controlador a nuestro nav
+        NavigationUI.setupWithNavController(btmNav, navControler)
+    }
+}
